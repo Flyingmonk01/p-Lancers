effaf4688d5051eaea7aa721c0b13d33990d4180
Sameer rai <sameerrai350.com>
2024-07-04 21:47:07 +0530

    refine

--- src/components/InfiniteMoving.tsx
+++ src/components/InfiniteMoving.tsx
@@ -2,29 +2,38 @@
 
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { FaArrowUp } from "react-icons/fa";
+
+type ArrayObject = {
+  id: string;
+  name: string;
+  email: string;
+  message: string;
+};
 
 export function InfiniteMovingCardsDemo() {
-    const [testimonials, setTestimonials] = useState([]);
-    useEffect(() => {
-        const getData = async () => {
-            const res = await fetch(
-                "https://p-lancers-default-rtdb.firebaseio.com/userMessageData.json",
-                {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-            const data = await res.json();
-            const arrayOfObjects = Object.keys(data).map((key) => ({
-                id: key,
-                ...data[key],
-            }));
-            setTestimonials(arrayOfObjects);
-        };
-        getData();
-    }, [testimonials]);
+  const [testimonials, setTestimonials] = useState<ArrayObject[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(
+        "https://p-lancers-default-rtdb.firebaseio.com/userMessageData.json",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      const arrayOfObjects = Object.keys(data).map((key) => ({
+        id: key,
+        ...data[key],
+      }));
+      setTestimonials(arrayOfObjects);
+    };
+    getData();
+  }, []); // Fetch data only once on mount
 
     return (
         <div className="h-[25rem] mb-10 rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
