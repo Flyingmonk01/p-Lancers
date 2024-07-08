f2cfd54b6926f4b25cc5bcdbb87ad3353935d35e
Sameer rai <sameerrai350.com>
2024-07-08 11:36:08 +0530

    hamburger menu created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { HoveredLink, Menu, MenuItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
@@ -9,7 +9,13 @@ import Logo from "../assets/p-Lancers.png";
 import { FiMenu } from "react-icons/fi";
 
 function Navbar({ className }: { className?: string }) {
+    const ref = useRef();
     const [active, setActive] = useState<string | null>(null);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isOpenInner, setIsOpenInner] = useState<boolean>(false);
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div
@@ -18,18 +24,18 @@ function Navbar({ className }: { className?: string }) {
                 className
             )}
         >
-            <div className="flex justify-between items-center bg-transparent max-w-6xl mx-auto">
+            <div className="flex justify-between items-center bg-transparent max-w-6xl mx-auto ">
                 {/* Logo on the left */}
                 <Link href="/">
                     <Image
                         src={Logo}
                         alt="Your Company Logo"
                         width={180}
-                        className="cursor-pointer z-20 fixed rounded-2xl -my-5"
+                        className="cursor-pointer z-20 fixed rounded-2xl "
                     />
                 </Link>
 
-                <Menu  setActive={setActive}>
+                <Menu setActive={setActive}>
                     <Link href="/">
                         <MenuItem
                             active={active}
@@ -72,13 +78,51 @@ function Navbar({ className }: { className?: string }) {
                     </Link>
                 </Menu>
 
-                <Link href="/contact" className="my-4">
-                    <MenuItem
-                        active={active}
-                        setActive={setActive}
-                        item="Contact Us"
-                    />
-                </Link>
+                <FiMenu
+                    className="fixed top-7 border text-2xl p-1 rounded-full right-7 z-50"
+                    onClick={toggleDrawer}
+                />
+
+                {isOpen && (
+                    <div
+                        onClick={toggleDrawer}
+                        className="bg-black border h-auto w-auto absolute top-20 right-0 rounded-lg z-99 px-2 py-4"
+                    >
+                        <Link href={"/"} children="Home" />
+
+                        <div >
+                        <MenuItem
+                            active={active}
+                            setActive={setActive}
+                            item="Our Services"
+                        >
+                            {isOpenInner && (
+                                <div
+                                    className={
+                                        "flex flex-col text-sm text-white space-y-4"
+                                    }
+                                    
+                                >
+                                    <HoveredLink  href="/web-dev">
+                                        Web Development
+                                    </HoveredLink>
+                                    <HoveredLink href="/data-analytics">
+                                        Data Analytics
+                                    </HoveredLink>
+                                    <HoveredLink href="/machine-learning">
+                                        Machine Learning
+                                    </HoveredLink>
+                                    <HoveredLink href="/mobile-development">
+                                        Android Development
+                                    </HoveredLink>
+                                </div>
+                            )}
+                        </MenuItem>
+                        </div>
+
+                        <Link href={"/contact"} children="Contact Us" />
+                    </div>
+                )}
             </div>
         </div>
     );
